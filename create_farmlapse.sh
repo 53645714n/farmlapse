0b52c4aa57db235352a969d01e7f466d47703c21
Daniël Schoenmaker <44052459+53645714n@users.noreply.github.com>
2021-03-11 23:12:02 +0100

    Update create_farmlapse.sh
    added default file override

--- create_farmlapse.sh
+++ create_farmlapse.sh
@@ -3,6 +3,6 @@
 #Set Path for ffmpeg Libraries
 export LD_LIBRARY_PATH=/usr/lib/
 
-ffmpeg -framerate 15 -pattern_type glob -i "/home/pi/pictures/*.jpg" -s:v 1440x1080 -c:v libx264 -crf 17 -pix_fmt yuv420p /home/pi/farmlapse.mp4
+ffmpeg -y -framerate 15 -pattern_type glob -i "/home/pi/pictures/*.jpg" -s:v 1440x1080 -c:v libx264 -crf 17 -pix_fmt yuv420p /home/pi/farmlapse.mp4
 
 #EOF
