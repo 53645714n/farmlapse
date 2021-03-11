69a02105c128f0e7da55d63f7f46b7e3d06a3b61
53645714n <schoenmaker@mailbox.org>
2021-03-11 22:56:14 +0100

    added script for timelapse

---
+++ create_farmlapse.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Set Path for ffmpeg Libraries
+export LD_LIBRARY_PATH=/usr/lib/
+
+ffmpeg -framerate 15 -pattern_type glob -i "/home/pi/pictures/*.jpg" -s:v 1440x1080 -c:v libx264 -crf 17 -pix_fmt yuv420p /home/pi/farmlapse.mp4
+
+#EOF
